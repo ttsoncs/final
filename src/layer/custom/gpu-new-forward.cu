fefe89f5e62e143c314070e6872c5eaef2a3c461
ttsoncs <tson25102002@gmail.com>
2024-01-01 22:48:11 +0700

    .

--- src/layer/custom/gpu-new-forward.cu
+++ src/layer/custom/gpu-new-forward.cu
@@ -4,12 +4,62 @@
 
 #define TILE_WIDTH 16
 
-__global__ void conv_forward_kernel(float *y, const float *x, const float *k,
-                                    const int B, const int M, const int C,
-                                    const int H, const int W, const int K) {
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(1);\
+    }\
+}
 
-  const int H_out = H - K + 1;
-  const int W_out = W - K + 1;
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+        cudaEventSynchronize(start); 
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
+
+__global__ void conv_forward_kernel(float *d_output, const float *d_input, const float *d_weight,
+                                    const int n_sample, const int channel_in, const int channel_out,
+                                    const int height_in, const int width_in, const int height_kernel) {
+
+  const int height_out = height_in - height_kernel + 1;
+  const int width_out = width_in - height_kernel + 1;
 
   // An example use of these macros:
   // float a = y4d(0,0,0,0)
@@ -21,109 +71,103 @@ __global__ void conv_forward_kernel(float *y, const float *x, const float *k,
 #define k4d(i3, i2, i1, i0)                                                    \
   k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-  int H_grid = ceil(1.0 * H_out / TILE_WIDTH);
-  int W_grid = ceil(1.0 * W_out / TILE_WIDTH);
+  int height_grid = ceil(1.0 * height_out / TILE_WIDTH);
+  int width_grid = ceil(1.0 * width_out / TILE_WIDTH);
 
   int b = blockIdx.x; // batch number
   int m = blockIdx.y; // output feature
-  int h = (blockIdx.z / W_grid) * TILE_WIDTH +
+  int h = (blockIdx.z / width_grid) * TILE_WIDTH +
           threadIdx.y; // row of the image matrix
-  int w = (blockIdx.z % W_grid) * TILE_WIDTH +
+  int w = (blockIdx.z % width_grid) * TILE_WIDTH +
           threadIdx.x; // col of the image matrix
 
   float accum = 0.0f;
 
-  if (h < H_out && w < W_out) {
-    for (int c = 0; c < C; c++) // sum over all input features
+  if (h < height_out && w < width_out) {
+    for (int c = 0; c < channel_in; c++) // sum over all input features
     {
-      for (int p = 0; p < K; p++) // KxK filter
-        for (int q = 0; q < K; q++)
+      for (int p = 0; p < height_kernel; p++) // KxK filter
+        for (int q = 0; q < height_kernel; q++)
           accum += x4d(b, c, h + p, w + q) *
                    k4d(m, c, p, q); // 4 dimensions macro resolve thread index
     }
     y4d(b, m, h, w) = accum;
-  } // endif (h < H_out && w < W_out)
+  }
 
 #undef y4d
 #undef x4d
 #undef k4d
 }
 
-__host__ void GPUInterface::conv_forward_gpu_prolog(
-    const float *host_y, const float *host_x, const float *host_k,
-    float **device_y_ptr, float **device_x_ptr, float **device_k_ptr,
-    const int B, const int M, const int C, const int H, const int W,
-    const int K) {
+void GPUInterface::conv_forward_gpu_prolog(
+    const float *output, const float *input,
+                               const float *weight, float **d_output,
+                               float **d_input, float **d_weight,
+                               const int n_sample, const int channel_out, const int channel_in,
+                               const int height_in, const int width_in, const int height_kernel) {
   // Allocate memory and copy over the relevant data structures to the GPU
 
   // We pass double pointers for you to initialize the relevant device pointers,
   //  which are passed to the other two functions.
 
-  const int H_out = H - K + 1;
-  const int W_out = W - K + 1;
+  const int height_out = height_in - height_kernel + 1;
+  const int width_out = width_in - height_kernel + 1;
 
-  int inputSize = B * C * H * W * sizeof(float); // input features map is C
-  int outputSize =
-      B * M * H_out * W_out * sizeof(float);    // output feature map is M
-  int maskSize = M * C * K * K * sizeof(float); // C * M filter Maps of size K*K
+  int inputSize = n_sample * channel_in * width_in * height_in * sizeof(float); // input features map is C
+  int outputSize = n_sample * channel_out * height_out * width_out * sizeof(float);    // output feature map is M
+  int maskSize = channel_out * channel_in * height_kernel * height_kernel * sizeof(float); // C * M filter Maps of size K*K
 
-  cudaMalloc((void **)device_x_ptr, inputSize);
-  cudaMalloc((void **)device_y_ptr, outputSize);
-  cudaMalloc((void **)device_k_ptr, maskSize);
+  CHECK(cudaMalloc(d_input, inputSize));
+  CHECK(cudaMalloc(d_output, outputSize));
+  CHECK(cudaMalloc(d_weight, maskSize));
 
-  // Copy Inout data to device
-  cudaMemcpy(*device_x_ptr, host_x, inputSize, cudaMemcpyHostToDevice);
+  // Copy Input data to device
+  CHECK(cudaMemcpy(*d_input, input, inputSize, cudaMemcpyHostToDevice));
+
   // Copy Mask data to device
-  cudaMemcpy(*device_k_ptr, host_k, maskSize, cudaMemcpyHostToDevice);
-
-  // Useful snippet for error checking
-  // cudaError_t error = cudaGetLastError();
-  // if(error != cudaSuccess)
-  // {
-  //     std::cout<<"CUDA error: "<<cudaGetErrorString(error)<<std::endl;
-  //     exit(-1);
-  // }
+  CHECK(cudaMemcpy(*d_weight, weight, maskSize, cudaMemcpyHostToDevice));
 }
 
-__host__ void GPUInterface::conv_forward_gpu(
-    float *device_y, const float *device_x, const float *device_k, const int B,
-    const int M, const int C, const int H, const int W, const int K) {
+void GPUInterface::conv_forward_gpu(
+    float *d_output, const float *d_input,
+                        const float *d_weight, const int n_sample, const int channel_out,
+                        const int channel_in, const int height_in, const int width_in, const int height_kernel) {
   // Set the kernel dimensions and call the kernel
 
-  const int H_out = H - K + 1;
-  const int W_out = W - K + 1;
+  const int height_out = height_in - height_kernel + 1;
+  const int width_out = width_in - height_kernel + 1;
 
-  int H_grid = ceil(1.0 * H_out / TILE_WIDTH);
-  int W_grid = ceil(1.0 * W_out / TILE_WIDTH);
-  int Z = H_grid * W_grid;
+  int X = ceil(1.0 * height_out / TILE_WIDTH);
+  int Y = ceil(1.0 * width_out / TILE_WIDTH);
+  int Z = X * Y;
 
   // Block dimensions = #of threads in the block
-  dim3 numThreadsPerBlock(TILE_WIDTH, TILE_WIDTH, 1);
+  dim3 blockSize(TILE_WIDTH, TILE_WIDTH, 1);
 
   // Grid Dimension = #of Blocks: Batch Size * Num_Output_Features *
-  dim3 numBlocksInGrid(B, M, Z);
+  dim3 gridSize(X, Y, Z);
 
   // launch the kernel
-  conv_forward_kernel<<<numBlocksInGrid, numThreadsPerBlock>>>(
-      device_y, device_x, device_k, B, M, C, H, W, K);
+  conv_forward_kernel<<<gridSize, blockSize>>>(
+      d_output, d_input, d_weight, n_sample, channel_out, channel_in, height_in, width_in, height_kernel);
 }
 
-__host__ void
-GPUInterface::conv_forward_gpu_epilog(float *host_y, float *device_y,
-                                      float *device_x, float *device_k,
-                                      const int B, const int M, const int C,
-                                      const int H, const int W, const int K) {
+void
+GPUInterface::conv_forward_gpu_epilog(float *output, float *d_output, float *d_input,
+                               float *d_weight, const int n_sample, const int channel_out,
+                               const int channel_in, const int height_in, const int width_in,
+                               const int height_kernel) {
   // Copy the output back to host
 
-  const int H_out = H - K + 1;
-  const int W_out = W - K + 1;
+  const int height_out = height_in - height_kernel + 1;
+  const int width_out = width_in - height_kernel + 1;
 
-  int outputSize = B * M * H_out * W_out * sizeof(float);
+  int outputSize = n_sample * channel_out * height_out * width_out * sizeof(float);
 
-  cudaMemcpy(host_y, device_y, outputSize, cudaMemcpyDeviceToHost);
+  CHECK(cudaMemcpy(output, d_output, outputSize, cudaMemcpyDeviceToHost));
 
   // Free device memory
-  cudaFree(device_x);
-  cudaFree(device_y);
-  cudaFree(device_k);
+  CHECK(cudaFree(d_input));
+  CHECK(cudaFree(d_output));
+  CHECK(cudaFree(d_weight));
 }
